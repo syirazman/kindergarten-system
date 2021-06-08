96213ed448617bb07bcf503dcbcb69c52d09f118
syirazman <84331491+syirazman@users.noreply.github.com>
2021-06-08 22:08:05 +0800

    Update update_staff.php

--- Admin/action_query/update_staff.php
+++ Admin/action_query/update_staff.php
@@ -1,14 +1,4 @@
-<?php
-$username="root";  
-$password="";  
-$hostname = "localhost";  
-//connection string with database  
-$dbhandle = mysqli_connect($hostname, $username, $password)  
-or die("Unable to connect to MySQL");  
-echo "";  
-// connect with database  
-$selected = mysqli_select_db($dbhandle, "kindergarten")  
-or die("Could not select examples");
+<?php include("/User/db_conn.php");
 
 $id = $_POST['id'];
 $nama = $_POST['nama'];
@@ -21,7 +11,7 @@ $update = mysqli_query($dbhandle, "UPDATE profile SET name = '$nama', email = '$
 
 if($update===TRUE)
 {
-	header('Location: http://'.$_SERVER['SERVER_NAME'].'/kindergarten-system/Admin/Maklumatguru.php');
+	header('Location: Admin/Maklumatguru.php');
     exit;
 }						
 else
